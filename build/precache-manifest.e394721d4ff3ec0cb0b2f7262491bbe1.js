5b60a6a9054717b041ab02e42820d4adf1ce50d4
engine.lin <engine.lin@csnt.com.tw>
2020-03-03 15:26:46 +0800

    push build folder.

---
+++ build/precache-manifest.e394721d4ff3ec0cb0b2f7262491bbe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a72f7a69d8c1db794e85fe75e98a0cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b2ce785ad20ed6d68a",
+    "url": "/static/css/2.c5b05437.chunk.css"
+  },
+  {
+    "revision": "98b2ce785ad20ed6d68a",
+    "url": "/static/js/2.885d5317.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/static/js/2.885d5317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016c2960ad270cbff097",
+    "url": "/static/js/main.85e5f7b2.chunk.js"
+  },
+  {
+    "revision": "d0eb2b98cfbce5c76cae",
+    "url": "/static/js/runtime-main.9bd4d1ea.js"
+  }
+]);
